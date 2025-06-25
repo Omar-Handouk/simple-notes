46b1452723d7fbc482e774f441d40a219b221bb4
Omar Handouk <o.i.handouk@gmail.com>
2025-06-25 05:47:43 +0300

    Modified sequelize config

---
+++ config/database.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  'development': {
+    'dialect': 'sqlite',
+    'storage': './data/db.sqlite3'
+  },
+  'test': {
+    'username': 'root',
+    'password': null,
+    'database': 'database_test',
+    'host': '127.0.0.1',
+    'dialect': 'mysql'
+  },
+  'production': {
+    'dialect': 'mysql',
+    'host': process.env.DB_HOST,
+    'port': process.env.DB_PORT,
+    'username': process.env.DB_USER,
+    'password': process.env.DB_PASS,
+    'database': process.env.DB_NAME
+  }
+};
